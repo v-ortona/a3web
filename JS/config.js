343f2bc239d1b37d34a8e8de08c08419987413f6
v-ortona <vortona@gmail.com>
2022-12-07 15:37:13 -0500

    Updated team page to correct images

--- JS/config.js
+++ JS/config.js
@@ -10,9 +10,8 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Our Team: Where is Everyone From?',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'Our Team',
+    subtitle: 'Where is Everyone From?',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -21,7 +20,7 @@ var config = {
             hidden: false,
             title: 'Fleming College',
             image: 'Images/fleming.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'Bungi Consulting headquarters established in Lindsay, ON in 2022',
             location: {
                 center: [-78.7408268,44.3410783],
                 zoom: 14.5,
@@ -91,7 +90,7 @@ var config = {
         },
         {
             id: 'Vince',
-            alignment: 'fully',
+            alignment: 'right',
             hidden: false,
             title: 'Vince Ortona',
             image: 'Images/vince.jpg',
@@ -110,7 +109,7 @@ var config = {
         },
         {
             id: 'Francis',
-            alignment: 'fully',
+            alignment: 'left',
             hidden: false,
             title: 'Francis Soriano',
             image: 'Images/Francis.jpg',
@@ -120,7 +119,7 @@ var config = {
                 zoom: 12.5,
                 pitch: 50,
                 bearing: 0,
-                speed: 0.5,
+                speed: 1,
                 curve: 1
             },
             mapAnimation: 'flyTo',
